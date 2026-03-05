b0285f3f5787e547024b1ac6e660bdee1cbe3b8a
R.Ragulrathinasamy <ragul7122003@gmail.com>
2026-03-05 12:22:16 +0530

    Time: 1 ms (94.39%) | Memory: 62.1 MB (89.20%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+
+    while (left < right) {
+      
+        let width = right - left;
+        
+
+        let currentHeight = Math.min(height[left], height[right]);
+        
+     
+        let currentArea = width * currentHeight;
+        
+       
+        if (currentArea > maxWater) {
+            maxWater = currentArea;
+        }
+
+       
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxWater;
+};
