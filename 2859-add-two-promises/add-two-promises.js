60290c1184bb16d181254e59e290ba2063d1f4b7
R.Ragulrathinasamy <ragul7122003@gmail.com>
2026-03-09 15:29:20 +0530

    Time: 65 ms (7.85%) | Memory: 54.4 MB (46.61%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    // Promise.all executes both promises concurrently
+    const [val1, val2] = await Promise.all([promise1, promise2]);
+    
+    // Return the sum
+    return val1 + val2;
+};
+
+/**
+ * Example usage:
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
