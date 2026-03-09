1ac2cfeb693e8a2de838862c72889aeb68c20e8d
R.Ragulrathinasamy <ragul7122003@gmail.com>
2026-03-09 15:40:49 +0530

    Time: 73 ms (52.67%) | Memory: 64.5 MB (85.65%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    // Base case: if obj is null or not an object, return it (it's a primitive)
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    // Handle Arrays
+    if (Array.isArray(obj)) {
+        const compactArray = [];
+        for (const item of obj) {
+            const val = compactObject(item);
+            // Only push if the value (after potential recursion) is truthy
+            if (Boolean(val)) {
+                compactArray.push(val);
+            }
+        }
+        return compactArray;
+    }
+
+    // Handle Objects
+    const compactObj = {};
+    for (const key in obj) {
+        const val = compactObject(obj[key]);
+        // Only add key if the value (after potential recursion) is truthy
+        if (Boolean(val)) {
+            compactObj[key] = val;
+        }
+    }
+    return compactObj;
+};
