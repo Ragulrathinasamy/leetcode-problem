17d6d2f315025c2bbcb5d98eec3b79ba76104da1
R.Ragulrathinasamy <ragul7122003@gmail.com>
2026-04-07 12:37:08 +0530

    Time: 1 ms (99.94%) | Memory: 46.3 MB (87.40%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Use boolean arrays to track digits 1-9 (index 0-8)
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+
+                int num = board[r][c] - '1'; // Convert '1'-'9' to 0-8
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                // Check if number already exists in current row, column, or box
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
+                    return false;
+                }
+
+                // Mark the number as seen
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIndex][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
